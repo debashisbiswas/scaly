9b8c125c530ca480b95c64a758724930bce620e9
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2026-01-31 22:42:01 -0700

    name flow

--- app/name-flow.tsx
+++ app/name-flow.tsx
@@ -3,16 +3,15 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 import { TopBar } from "./components/TopBar";
 
-export default function ChooseRhythm() {
+export default function ChooseTempo() {
   const router = useRouter();
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <TopBar
-        title="Rhythm and Articulation"
-        subtitle="Select all that apply"
+        title="Name this Flow"
+        subtitle="This can be changed later"
         onBack={() => router.back()}
-        onNext={() => router.push("/name-flow")}
       />
     </SafeAreaView>
   );
